cde94f257a07111d08b6966e9d80c4e1461d7ace
Adam Gapinski <adam.gapinski1@gmail.com>
2017-03-18 15:32:24 +0100

    micro time span structure, constructor from two time points, destructor, calc_average based on 3 time spans

--- zad2/main.c
+++ zad2/main.c
@@ -43,6 +43,70 @@ typedef struct timePoint {
     struct timeval *utime;
 } timePoint;
 
+typedef struct micro_t_span {
+    double rtime;
+    double utime;
+    double stime;
+} micro_t_span;
+
+void delete_t_span(micro_t_span *span) {
+    free(span);
+}
+
+long getTimeDiffMicro(struct timeval *start, struct timeval *end) {
+    const long ONE_SEC_TO_MICRO = 1000000;
+    long result = 0;
+    result += end->tv_usec - start->tv_usec;
+    result += (end->tv_sec - start->tv_sec) * ONE_SEC_TO_MICRO;
+
+    return result;
+}
+
+micro_t_span *create_time_span(timePoint *start, timePoint *end) {
+    const int SEC_TO_MICRO = 1000000;
+    const int MICRO_TO_NANO = 1000;
+    micro_t_span *result = malloc(sizeof(*result));
+
+    double rtime, utime, stime;
+    rtime = utime = stime = 0;
+
+    /*setting utime and stime*/
+    utime = end->utime->tv_usec - start->utime->tv_usec;
+    stime = end->stime->tv_usec - start->stime->tv_usec;
+    utime += (end->utime->tv_sec - start->utime->tv_sec) * SEC_TO_MICRO; //converting seconds to microseconds
+    stime += (end->stime->tv_sec - start->stime->tv_sec) * SEC_TO_MICRO; //converting seconds to microseconds
+    result->utime = utime;
+    result->stime = stime;
+
+    /*setting rtime*/
+    rtime = end->rtime->tv_nsec - start->rtime->tv_nsec;
+    rtime /= MICRO_TO_NANO; //converting nanoseconds to microseconds
+    rtime += (end->rtime->tv_sec - start->rtime->tv_sec) * SEC_TO_MICRO; //converting seconds to microseconds
+    result->rtime = rtime;
+
+    return result;
+}
+micro_t_span *calc_average(micro_t_span *fst, micro_t_span *scd, micro_t_span *lst) {
+    micro_t_span *result = malloc(sizeof(*result));
+    double rtime, utime, stime;
+
+    rtime = fst->rtime + scd->rtime + lst->rtime;
+    rtime /= 3;
+
+    utime = fst->utime + scd->utime + lst->utime;
+    utime /= 3;
+
+    stime = fst->stime + scd->stime + lst->stime;
+    stime /= 3;
+
+    result->rtime = rtime;
+    result->utime = utime;
+    result->stime = stime;
+
+    return result;
+}
+
+
 timePoint *createTimePoint() {
     timePoint *point = malloc(sizeof(*point));
     point->rtime = malloc(sizeof(*point->rtime));
@@ -66,15 +130,6 @@ void deleteTimePoint(timePoint *point) {
     free(point->stime);
     free(point->utime);
     free(point);
-}
-
-long getTimeDiffMicro(struct timeval *start, struct timeval *end) {
-    const long ONE_SEC_TO_MICRO = 1000000;
-    long result = 0;
-    result += end->tv_usec - start->tv_usec;
-    result += (end->tv_sec - start->tv_sec) * ONE_SEC_TO_MICRO;
-
-    return result;
 }
 
 void endTimeMeausure(timePoint *start, char *note) {

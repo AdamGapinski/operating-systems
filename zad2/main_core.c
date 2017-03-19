0541786b8eaca252a235a5e3ecb0d3c1a50201a3
Adam Gapinski <adam.gapinski1@gmail.com>
2017-03-19 18:11:36 +0100

    Adjusted main_core.c and Makefile to previous changes

--- zad2/main_core.c
+++ zad2/main_core.c
@@ -5,7 +5,6 @@
 #include "../zad1/include/linkedlistbook.h"
 #include "../zad1/include/binarytreebook.h"
 #include "include/t_measurement.h"
-#include "include/measurement_utils.h"
 
 const int MAX_LINE_LENGTH = 200;
 const int CONTACT_STR_FIELDS = 9;
@@ -105,6 +104,81 @@ linkedBook *create_linked_book(contactStr **contacts) {
     }
 
     return book;
+}
+
+typedef linkedBook* (*on_contacts_m_linked)(contactStr **contacts);
+typedef bTBook* (*on_contacts_m_tree)(contactStr **contacts);
+typedef void (*on_linked_book_m)(linkedBook *book);
+typedef void (*on_linked_book_ct_m)(linkedBook *book, contactStr *contact);
+typedef void (*on_tree_book_m)(bTBook *book);
+typedef void (*on_tree_book_m_ct_m)(bTBook *book, contactStr *contact);
+
+micro_t_span *on_contacts_m_linked_time(on_contacts_m_linked method, contactStr **contacts) {
+    timePoint *start = createTimePoint();
+    linkedBook *book = method(contacts);
+    timePoint *end = createTimePoint();
+
+    micro_t_span *result = create_time_span(start, end);
+    deleteLinkedBook(&book);
+    deleteTimePoint(start);
+    deleteTimePoint(end);
+    return result;
+}
+
+micro_t_span *on_contacts_m_tree_time(on_contacts_m_tree method, contactStr **contacts) {
+    timePoint *start = createTimePoint();
+    bTBook *book = method(contacts);
+    timePoint *end = createTimePoint();
+
+    micro_t_span *result = create_time_span(start, end);
+    deleteBTBook(&book);
+    deleteTimePoint(start);
+    deleteTimePoint(end);
+    return result;
+}
+
+micro_t_span *on_linked_book_m_time(on_linked_book_m method, linkedBook *book) {
+    timePoint *start = createTimePoint();
+    method(book);
+    timePoint *end = createTimePoint();
+
+    micro_t_span *result = create_time_span(start, end);
+    deleteTimePoint(start);
+    deleteTimePoint(end);
+    return result;
+}
+
+micro_t_span *on_linked_book_ct_m_time(on_linked_book_ct_m method, linkedBook *book, contactStr *contact) {
+    timePoint *start = createTimePoint();
+    method(book, contact);
+    timePoint *end = createTimePoint();
+
+    micro_t_span *result = create_time_span(start, end);
+    deleteTimePoint(start);
+    deleteTimePoint(end);
+    return result;
+}
+
+micro_t_span *on_tree_book_m_time(on_tree_book_m method, bTBook *book) {
+    timePoint *start = createTimePoint();
+    method(book);
+    timePoint *end = createTimePoint();
+
+    micro_t_span *result = create_time_span(start, end);
+    deleteTimePoint(start);
+    deleteTimePoint(end);
+    return result;
+}
+
+micro_t_span *on_tree_book_ct_m_time(on_tree_book_m_ct_m method, bTBook *book, contactStr *contact) {
+    timePoint *start = createTimePoint();
+    method(book, contact);
+    timePoint *end = createTimePoint();
+
+    micro_t_span *result = create_time_span(start, end);
+    deleteTimePoint(start);
+    deleteTimePoint(end);
+    return result;
 }
 
 void measure_linkbook_creation(contactStr **contacts) {

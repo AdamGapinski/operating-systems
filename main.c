1bba6c88b45e0bba75d5b8bf65a0fb47c472ee12
Adam Gapinski <adam.gapinski1@gmail.com>
2017-03-25 11:44:07 +0100

    jmp solution

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <wait.h>
+#include <setjmp.h>
 #include "scanner.h"
 
 void read_lines(FILE *fp) ;
@@ -14,9 +15,11 @@ void execute_line(char *buff) ;
 
 void assign_env(char *var, token_buff *buff);
 
+void handle_jmp(int jmp, char *line_buff, int line_num) ;
+
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "Error: Wrong number of arguments specified. Specify batch program file name\n");
+        fprintf(stderr, "Error: Wrong number of arguments. Specify file name of batch file\n");
         exit(EXIT_FAILURE);
     }
 
@@ -26,15 +29,29 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+jmp_buf jmp_buff;
+
 void read_lines(FILE *fp) {
     size_t line_len = 256;
-    char *line_buff = calloc(line_len, sizeof(*line_buff));
 
-    while (getline(&line_buff, &line_len, fp) != -1) {
+    char *line_buff = calloc(line_len, sizeof(*line_buff));
+    for (int line_num = 1; getline(&line_buff, &line_len, fp) != -1; ++line_num) {
+        handle_jmp(setjmp(jmp_buff), line_buff, line_num);
         execute_line(line_buff);
     }
 
-    free(line_buff);
+    free((void *) line_buff);
+}
+
+void handle_jmp(int jmp, char *line_buff, int line_num) {
+    if (jmp > 0) {
+        fprintf(stderr, "Error occurred in %d. line: \"%s\" process exit status: %d\n",
+                line_num, strtok(line_buff, "\n"), jmp);
+        exit(EXIT_FAILURE);
+    } else if (jmp < 0) {
+        fprintf(stderr, "Unexpected error occurred in %d line: %s\n", line_num, strtok(line_buff, "\n"));
+        exit(EXIT_FAILURE);
+    }
 }
 
 //this method will leave the first element of the returned array free
@@ -57,6 +74,16 @@ void remove_argv(char **argv) {
     free(argv);
 }
 
+void read_status(int status) {
+    if (WIFEXITED(status)) {
+        if (WEXITSTATUS(status) != 0){
+            longjmp(jmp_buff, WEXITSTATUS(status));
+        }
+    } else {
+        longjmp(jmp_buff, -1);
+    }
+}
+
 void process(char *filename, token_buff *buff) {
     //create_argv method returns an array of pointers, starting from it' s second index
     char **argv = create_argv(buff);
@@ -70,7 +97,9 @@ void process(char *filename, token_buff *buff) {
         //takes filename of the executable file and search for it in the directories specified by PATH env variable.
         execvp(filename, argv);
     } else {
-        wait(NULL);
+        int stat = 0;
+        wait(&stat);
+        read_status(stat);
         //we do not have to free filename, because pointer to filename is in the argv[0]
         remove_argv(argv);
     }

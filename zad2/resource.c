82bcff031bf0492697e02528c3c9cb151b69eb8e
Adam Gapinski <adam.gapinski1@gmail.com>
2017-03-26 18:33:23 +0200

    resource program has to exceed resource limits passed to main program. Modified Makefile to easier the debug

--- zad2/resource.c
+++ zad2/resource.c
@@ -1,3 +1,10 @@
-int main(int argc, char **argv) {
+#include <stdlib.h>
 
+int main() {
+    for (int i = 0; i < 100000; ++i) {
+        int *tab = malloc(100);
+        for (int j = 0; j < 100; ++j) {
+            tab[j] = 34;
+        }
+    }
 }

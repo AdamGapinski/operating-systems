ad6242c19b49122b1ff502dd30d82e28514991ce
Adam Gapinski <adam.gapinski1@gmail.com>
2017-04-03 13:01:10 +0200

    before rt signals

--- zad2/main.c
+++ zad2/main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <bits/siginfo.h>
+#include <wait.h>
 
 void fork_processes(int processes) ;
 
@@ -11,7 +12,7 @@ void set_signal_handlers() ;
 
 void process();
 
-int request_received;
+int requests_received;
 int exp_requests;
 int child_processes;
 
@@ -37,7 +38,6 @@ int main(int argc, char *argv[]) {
     exp_requests = requests;
     child_processes = processes;
 
-    srand48(time(NULL));
     set_signal_handlers();
     fork_processes(processes);
     return 0;
@@ -58,43 +58,64 @@ void fork_processes(int processes) {
 
 
 void process() {
-    int time = (int) (drand48() * 11);
-    sleep((unsigned int) time);
+    //todo set time randomization
+    srand48(time(NULL));
+    int sleep_time = (int) (drand48() * 11);
+    sleep((unsigned int) sleep_time);
+    printf("PID = %d, randomized %d time\n", getpid(), sleep_time);
 
     //sending request
     union sigval usr_sigval;
     usr_sigval.sival_int = getpid();
     struct timespec start;
     clock_gettime(CLOCK_MONOTONIC, &start);
-    sigqueue(getppid(), SIGUSR1, usr_sigval);
+    printf("PID = %d send usr1 sig\n", getpid());
+    if (sigqueue(getppid(), SIGUSR1, usr_sigval) != 0) {
+        perror("Error:");
+    }
 
+    printf("PID = %d stopped\n", getpid());
     //waiting for response
     raise(SIGSTOP);
+    printf("PID = %d continued\n", getpid());
 
     //ending job
     int signal_no = SIGRTMIN + (int) (drand48() * (SIGRTMAX - SIGRTMIN + 1));
     union sigval rt_sigval;
     rt_sigval.sival_int = getpid();
-    sigqueue(getppid(), signal_no, rt_sigval);
+    printf("PID = %d send real time sig\n", getpid());
+    if (sigqueue(getppid(), signal_no, rt_sigval) != 0) {
+        perror("Error");
+    }
 
     clock_gettime(CLOCK_MONOTONIC, &start);
     struct timespec end;
 
     //exiting with time diff in seconds
+    printf("PID = %d ending\n", getpid());
     exit((int) (end.tv_sec - start.tv_sec));
 }
 
 void handle_request(int signo, siginfo_t *info, void *ptr) {
-    request_received++;
-    if (request_received >= exp_requests) {
+    ++requests_received;
+    if (requests_received >= exp_requests) {
+    printf("PID = %d received usr1 from %d\n", getpid(), info->si_value.sival_int);
         union sigval sigval;
-        sigqueue(info->si_value.sival_int, SIGCONT, sigval);
+        printf("PID = %d sending sigcont to %d\n", getpid(), info->si_value.sival_int);
+        if (sigqueue(info->si_value.sival_int, SIGCONT, sigval) != 0) {
+            perror("Error");
+        }
+        printf("PID = %d sent sigcont to %d\n", getpid(), info->si_value.sival_int);
     }
 }
 
 void handle_child_exit(int signo, siginfo_t *info, void *ptr) {
-    child_processes--;
+    printf("PID = %d received rt sig from %d\n", getpid(), info->si_value.sival_int);
+    --child_processes;
+    printf("PID = %d subtracting to %d\n", getpid(), child_processes);
     if (child_processes == 0) {
+        wait(NULL);
+        printf("PID = %d waited for %d\n", getpid(), info->si_value.sival_int);
         exit(EXIT_SUCCESS);
     }
 }
@@ -104,15 +125,20 @@ void set_signal_handlers() {
     struct sigaction usr_action;
     usr_action.sa_sigaction = handle_request;
     usr_action.sa_flags |= SA_SIGINFO;
+    //usr_action.sa_flags |= SA_NODEFER;
     sigaction(SIGUSR1, &usr_action, NULL);
 
     struct sigaction rt_action;
-    usr_action.sa_sigaction = handle_child_exit;
-    usr_action.sa_flags |= SA_SIGINFO;
+    rt_action.sa_sigaction = handle_child_exit;
+    rt_action.sa_flags |= SA_SIGINFO;
 
+    //todo rt_set may be redundant
+    sigset_t rt_set;
+    sigemptyset(&rt_set);
     int sig_no = SIGRTMIN;
     while (sig_no <= SIGRTMAX) {
         sigaction(sig_no, &rt_action, NULL);
+        sigaddset(&rt_set, sig_no);
         sig_no++;
     }
 }

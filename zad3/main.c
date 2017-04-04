3a59de1445b280a6221643bbf2538bd6da2f86a7
Adam Gapinski <adam.gapinski1@gmail.com>
2017-04-04 03:47:57 +0200

    next

--- zad3/main.c
+++ zad3/main.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <memory.h>
 
+void send_signals(int signals, int type, int process_id) ;
+
+void set_process_handlers();
+
 void log(char *msg, int parent_id, int pid, int slp_flag) {
     char *msg_cpy = calloc(500, sizeof(*msg_cpy));
     sprintf(msg_cpy, "%d\t%s\t%d\n", parent_id, msg, pid);
@@ -15,7 +19,63 @@ void log(char *msg, int parent_id, int pid, int slp_flag) {
     free(msg_cpy);
 }
 
-int main() {
-    log("test", 1,1 ,0);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Error: Wrong number of arguments. Specify two arguments: L - number of signals and Type (1, 2, 3)\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int signals;
+    if (sscanf(argv[1], "%d", &signals) != 1) {
+        fprintf(stderr, "Error: While parsing L - number of signals\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int type;
+    if (sscanf(argv[2], "%d", &type) != 1 || (type != 1 && type != 2 && type != 3)) {
+        fprintf(stderr, "Error: While parsing Type\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int process_id;
+    if ((process_id = fork()) == 0) {
+        set_process_handlers();
+        while(1);
+    } else {
+        set_hanlders();
+        send_signals(signals, type, process_id);
+    }
     return 0;
+}
+
+void set_process_handlers() {
+    struct sigaction usr_action;
+    usr_action.sa_sigaction = handle_usr1;
+    sigaction(SIGUSR1, &usr_action, NULL);
+
+    struct sigaction usr_action;
+    usr_action.sa_sigaction = handle_usr1;
+    sigaction(SIGUSR1, &usr_action, NULL);
+}
+
+void send_signals(int signals, int type, int process_id) {
+    if (type == 1) {
+        for (int i = 0; i < signals; ++i) {
+            kill(process_id, SIGUSR1);
+        }
+
+        kill(process_id, SIGUSR2);
+    } else if (type == 2) {
+        for (int i = 0; i < signals; ++i) {
+            sigqueue(process_id, SIGUSR1, NULL);
+        }
+
+        sigqueue(process_id, SIGUSR2, NULL);
+    } else if (type == 3) {
+        for (int i = 0; i < signals; ++i) {
+            kill(process_id, SIGRTMIN);
+        }
+
+        kill(process_id, SIGRTMIN + 1);
+    }
 }

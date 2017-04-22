e8dd6f2a65b1a8f155d2d9196ec225a4ec280135
Adam Gapinski <adam.gapinski1@gmail.com>
2017-04-22 23:29:29 +0200

    Created common.h header file for Client and Server

---
+++ zad1/common.h
@@ -0,0 +1,10 @@
+//
+// Created by adam on 22.04.17.
+//
+
+#ifndef IPCCOMMUNICATION_COMMON_H
+#define IPCCOMMUNICATION_COMMON_H
+typedef enum {
+    REGISTER, ECHO, CAPS, TIME, EXIT
+} Request;
+#endif //IPCCOMMUNICATION_COMMON_H

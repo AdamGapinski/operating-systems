b04528cb79401440f27922ac5d91418745a654c7
Adam Gapinski <adam.gapinski1@gmail.com>
2017-04-23 03:46:05 +0200

    Added message structure in common.h. Defined necessary consts

--- zad1/common.h
+++ zad1/common.h
@@ -4,7 +4,19 @@
 
 #ifndef IPCCOMMUNICATION_COMMON_H
 #define IPCCOMMUNICATION_COMMON_H
+
+const char PROJ_ID = 'm';
+
+#define MSG_MAX_SIZE 512
+
 typedef enum {
     REGISTER, ECHO, CAPS, TIME, EXIT
 } Request;
+
+typedef struct message {
+    long message_type;
+    char message[MSG_MAX_SIZE];
+    pid_t client;
+} message;
+
 #endif //IPCCOMMUNICATION_COMMON_H

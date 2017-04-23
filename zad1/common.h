c6eb3c828137c71c63d36c375fce24151afb6b2c
Adam Gapinski <adam.gapinski1@gmail.com>
2017-04-23 11:29:27 +0200

    Added seeds for ftok keys in common.h header

--- zad1/common.h
+++ zad1/common.h
@@ -7,10 +7,17 @@
 
 const char PROJ_ID = 'm';
 
+#define MAX_CLIENTS 20
+#define KEY_SEEDS_SIZE 25
+
+const char KEY_SEEDS[KEY_SEEDS_SIZE] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+                                        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
+                                        'n', 'Q', 'W', 'E', 'R'};
+
 #define MSG_MAX_SIZE 512
 
 typedef enum {
-    REGISTER, ECHO, CAPS, TIME, EXIT
+    UNDEFINED, REGISTER, ECHO, CAPS, TIME, EXIT
 } Request;
 
 typedef struct message {

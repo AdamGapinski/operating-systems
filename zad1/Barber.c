f37bb3ec1078b943478f4110670a7d46c067c7d0
Adam Gapinski <adam.gapinski1@gmail.com>
2017-05-04 02:40:58 +0200

    Implemented ClientsQueue initialization and basic queue operations - enqueue and deque

--- zad1/Barber.c
+++ zad1/Barber.c
@@ -2,12 +2,96 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <memory.h>
+#include <signal.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
 #include "include/Common.h"
 
 int parseClientsQueueSize(int argc, char *argv[]) ;
 
+void clean();
+
+void sigintExitHandler(int sig) ;
+
+void *getAdr(int i);
+
+int enqueue(int *queue, int *head, int *queued, int size, int value) ;
+
+int dequeue(int *queue, int *head, int *queued, int size) ;
+
+void initClientsQueue(int size) ;
+
+ClientsQueue *clientsQueue;
+
 int main(int argc, char *argv[]) {
+    signal(SIGINT, sigintExitHandler);
+    atexit(clean);
     int size = parseClientsQueueSize(argc, argv);
+    initClientsQueue(size);
+}
+
+int enqueue(int *queue, int *head, int *queued, int size, int value) {
+    if (*queued == size) {
+        return -1;
+    }
+    queue[(*head + *queued) % size] = value;
+    *queued += 1;
+    return 0;
+}
+
+int dequeue(int *queue, int *head, int *queued, int size) {
+    if (*queued == 0) {
+        return -1;
+    }
+    int result = queue[*head];
+    *head = (*head + 1) % size;
+    *queued -= 1;
+    return result;
+}
+
+void initClientsQueue(int size) {
+    /*
+     * getAdr will return a pointer to shared memory, which will be mapped to ClientsQueue structure,
+     * but besides that, the memory will also contain array of ints, allocated after this structure.
+     * */
+    int sizeOfArray = size * sizeof(*(*clientsQueue).queue);
+    clientsQueue = (ClientsQueue *) getAdr(sizeof(*clientsQueue) + sizeOfArray);
+    clientsQueue->queue = (void *) clientsQueue + sizeof(*clientsQueue);
+    clientsQueue->head = 0;
+    clientsQueue->queued = 0;
+    clientsQueue->size = size;
+}
+
+void *getAdr(int size) {
+    int key;
+    if ((key = ftok("./", CLIENTS_QUEUE_KEY)) == -1) {
+        perror("getting key");
+        exit(EXIT_FAILURE);
+    };
+    int shId;
+
+    errno = 0;
+    if ((shId = shmget(key, (size_t) size, IPC_CREAT | 0666)) == -1) {
+        perror("getting shared memory id");
+        exit(EXIT_FAILURE);
+    }
+
+    void *result;
+    if ((result = shmat(shId, NULL, 0)) == (void *) -1) {
+        perror("attaching memory");
+        exit(EXIT_FAILURE);
+    }
+    return result;
+}
+
+void sigintExitHandler(int sig) {
+    exit(EXIT_SUCCESS);
+}
+
+void clean() {
+    shmdt(clientsQueue);
+    shmctl(shmget(ftok("./", CLIENTS_QUEUE_KEY), 0, 0), IPC_RMID, NULL);
 }
 
 int parseClientsQueueSize(int argc, char *argv[]) {
--- zad1/include/Common.h
+++ zad1/include/Common.h
@@ -5,4 +5,11 @@
 #ifndef SHAREDMEMORYSEM_COMMON_H
 #define SHAREDMEMORYSEM_COMMON_H
 
+const int CLIENTS_QUEUE_KEY = 23;
+struct ClientsQueue {
+    int *queue;
+    int head;
+    int queued;
+    int size;
+} typedef ClientsQueue;
 #endif //SHAREDMEMORYSEM_COMMON_H
